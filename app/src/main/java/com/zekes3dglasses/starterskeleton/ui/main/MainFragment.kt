11d8d1258ca229843754e7b7fde981a774dee5fb
ryanvogt <vogtrj@gmail.com>
2020-02-16 09:08:25 -0700

    Implemented: -LiveData with a backing field and transformation -Button with a bound clicklistener

--- app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainFragment.kt
+++ app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.zekes3dglasses.starterskeleton.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.zekes3dglasses.starterskeleton.R
 import com.zekes3dglasses.starterskeleton.databinding.MainFragmentBinding
@@ -16,19 +16,39 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    // Declare the binding and viewModel objects for use later
+    private lateinit var binding: MainFragmentBinding
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        val binding = DataBindingUtil.inflate<MainFragmentBinding>(inflater,
-                R.layout.main_fragment, container,false)
-        return binding.root
-    }
+                              savedInstanceState: Bundle?
+    ): View? {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.main_fragment,
+            container,
+            false
+        )
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Initialize the viewModel so it can be bound in the next step
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        // Set the viewModel for databinding - this allows the bound layout access to
+        // all the data in the viewModel
+        binding.viewModel = viewModel
+
+        // Demonstration of setting a viewModel function to a fragment ClickListener
+        binding.buttonAddDemo.setOnClickListener {
+            viewModel.addToInt()
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainViewModel.kt
+++ app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainViewModel.kt
@@ -1,7 +1,24 @@
 package com.zekes3dglasses.starterskeleton.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
+    // Demonstration of LiveData being used with backing field
+    private val _sampleIntLiveData = MutableLiveData<Int>()
+    val sampleIntLiveData: LiveData<Int>
+        get() = _sampleIntLiveData
+
+    // The String version of the sampleIntLiveData, used by the fragment xml
+    val intInStringFormat = Transformations.map(sampleIntLiveData) {
+        it.toString()
+    }
+
+    fun addToInt() {
+        _sampleIntLiveData.value = _sampleIntLiveData.value?.plus(1) ?: 1
+    }
 }
