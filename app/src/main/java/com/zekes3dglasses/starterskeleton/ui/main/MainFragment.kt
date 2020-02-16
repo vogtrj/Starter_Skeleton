8d48518c708621bdb8e51b00f0347c9017932ee6
ryanvogt <vogtrj@gmail.com>
2020-02-15 17:09:00 -0700

    Initial commit

---
+++ app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.zekes3dglasses.starterskeleton.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.zekes3dglasses.starterskeleton.R
+import com.zekes3dglasses.starterskeleton.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding = DataBindingUtil.inflate<MainFragmentBinding>(inflater,
+                R.layout.main_fragment, container,false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/zekes3dglasses/starterskeleton/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.zekes3dglasses.starterskeleton.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
